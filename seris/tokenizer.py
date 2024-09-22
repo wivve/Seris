b8ec97b1240ea4f63d5e64e935e4a92d0e3c2a11
0x1F8F <86254906+0x1F8F@users.noreply.github.com>
2024-09-22 20:52:47 +0530

    fixed tokenizer [random noise]

--- seris/tokenizer.py
+++ seris/tokenizer.py
@@ -10,12 +10,14 @@ class Tokenizer:
     def __init__(self, special_token: Optional[List[str]] = None):
         self.kv = dict()
         self.vk = dict()
+        self.vs = 0
         if special_token is not None:
             self.__with_special_token(*special_token)
 
     def __with_special_token(self, *s_token: str) -> None:
         for i, t in enumerate(s_token):
-            self.kv[t] = i
+            self.kv[t] = self.vs
+            self.vs += 1
         self.__update_vk()
 
     def __update_vk(self) -> None:
@@ -26,14 +28,18 @@ class Tokenizer:
     def vocab_size(self) -> int:
         return len(self.kv)
 
-    def train(self, x: DataFrame | Series, index: Optional[str | int] = None) -> None:
-        if index is not None and isinstance(x, DataFrame):
-            x = x[index].clone()
-        assert isinstance(x, Series), "invalid datatype " + repr(type(x))
-        _data = set("".join(i for i in x.to_list()))
-        for i, j in enumerate(_data, start=self.vocab_size):
-            if self.kv.get(j) is None:
-                self.kv[j] = i
+    def train(self, x: DataFrame | Series | str, index: Optional[str | int] = None) -> None:
+        if isinstance(x,str):
+            _data = set(x)
+        else:
+            if index is not None and isinstance(x, DataFrame):
+                x = x[index].clone()
+            assert isinstance(x, Series), "invalid datatype " + repr(type(x))
+            _data = set("".join(i for i in x.to_list()))
+        for i in _data:
+            if self.kv.get(i) is None:
+                self.kv[i] = self.vs
+                self.vs += 1
         self.__update_vk()
 
     def encode(self, x: str) -> Tensor:
